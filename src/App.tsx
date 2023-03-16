106364abf9ff4cc68c533345285e92b9ee613c6e
Mikhail Barykin <mbarykin322@gmail.com>
2023-03-16 18:39:54 +0300

    Добавил classnames 8 урок

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
-import React, { Suspense, useContext, useState } from 'react';
+import React, { Suspense } from 'react';
 import {Link, Route, Routes} from 'react-router-dom';
 import './styles/index.scss';
 import {AboutPageAsync} from "./pages/AboutPage/AboutPage.async";
 import {MainPageAsync} from "./pages/MainPage/MainPage.async";
 import { useTheme } from './themeContext/useTheme';
+import {classNames} from "./helpers/classNames/classNames";
 
 const App = () => {
     const { theme, toggleTheme } = useTheme()
 
     return (
-        <div className={`app ${theme}`}>
+        <div className={classNames('app', {}, [theme])}>
             <button onClick={toggleTheme}>Toggle</button>
             <Link to={'/'}>Главная</Link>
             <Link to={'/about'}>О сайте</Link>
