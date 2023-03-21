3d62ca157d13a6a58e186e9f99c61be5a023d877
Mikhail Barykin <mbarykin322@gmail.com>
2023-03-21 10:50:17 +0300

    Добавил структуру FSD - 1 модуль последний урок

--- config/build/buildResolvers.ts
+++ config/build/buildResolvers.ts
@@ -1,7 +1,11 @@
+import { BuildOptions } from './types/config';
 import {ResolveOptions} from "webpack";
 
-export function buildResolvers(): ResolveOptions {
+export function buildResolvers(options: BuildOptions): ResolveOptions {
     return {
         extensions: ['.tsx', '.ts', '.js'],
+        alias: {},
+        mainFiles: ['index'],
+        modules: [options.paths.src, 'node_modules']
     }
 }
